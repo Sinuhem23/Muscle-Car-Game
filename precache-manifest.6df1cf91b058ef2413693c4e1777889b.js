86fa04ee808a48c8ec277e8418f7e97082174105
sinuhe Montero <sinuhem23@gmail.com>
2019-12-04 00:34:18 -0500

    Updates

---
+++ precache-manifest.6df1cf91b058ef2413693c4e1777889b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5d4fa63585e5e1e8f223152095f417",
+    "url": "/Muscle-Car-Game/index.html"
+  },
+  {
+    "revision": "d1aee30dcb0452b7d436",
+    "url": "/Muscle-Car-Game/static/css/main.a8216768.chunk.css"
+  },
+  {
+    "revision": "a8b0fd9fe25d04a400db",
+    "url": "/Muscle-Car-Game/static/js/2.a6d05585.chunk.js"
+  },
+  {
+    "revision": "d1aee30dcb0452b7d436",
+    "url": "/Muscle-Car-Game/static/js/main.10b9c872.chunk.js"
+  },
+  {
+    "revision": "43dc227ce3672ba375e3",
+    "url": "/Muscle-Car-Game/static/js/runtime-main.5b5ddf18.js"
+  }
+]);
